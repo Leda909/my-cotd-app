6b40a76b3393dd35bf78fd5bec0718842aefa5a0
Leda909 <ko9zad@gmail.com>
2021-04-05 20:25:22 +0100

    add form JSX to component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,13 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Hello!</p>
+        return (
+            <form className="store-selector">
+                <h2>Please enter into the store!</h2>
+                <input type="text" required pleaceholder="Store Name" />
+                <button type="submit">Visit Store </button>
+            </form>
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
 
+{/* comment test! */}
+
 render(<StorePicker/>, document.querySelector('#main'));

c05249a72da9b33975814a28659d077c47a2c5eb
Leda909 <ko9zad@gmail.com>
2021-04-05 19:57:37 +0100

    move class into components file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
 
-class StorePicker extends React.Component {
-    render() {
-        return <p>Hello!</p>
-    }
-}
-
-render(<p>Heyyyy!</p>, document.querySelector('#main'));
+render(<StorePicker/>, document.querySelector('#main'));
